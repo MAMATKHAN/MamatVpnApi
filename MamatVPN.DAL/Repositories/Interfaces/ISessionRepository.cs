01b95fd77327d18f671c825333b94bdc93cb627b
m.magomednabiev <m.magomednabiev>
2025-03-13 14:50:19 +0500

    Update repository interfaces

--- MamatVPN.DAL/Repositories/Interfaces/ISessionRepository.cs
+++ MamatVPN.DAL/Repositories/Interfaces/ISessionRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MamatVPN.DAL.Entites;
 
 namespace MamatVPN.DAL.Repositories.Interfaces
 {
-    internal interface ISessionRepository
+    public interface ISessionRepository
     {
+        Task<SessionEntityV1> Get(long sessionId, CancellationToken token);
+        Task<SessionEntityV1[]> GetAll(CancellationToken token);
+        Task<long> Create(SessionEntityV1 entity, CancellationToken token);
+        Task Update(SessionEntityV1 entity, CancellationToken token);
+        Task Delete(long sessionId, CancellationToken token);
     }
 }
